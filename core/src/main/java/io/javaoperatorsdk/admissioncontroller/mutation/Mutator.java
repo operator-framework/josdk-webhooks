21cdc426aa56569f978b6eb1dd720ea02980f0d7
csviri <csviri@gmail.com>
2021-12-21 16:55:23 +0100

    fix: annotation improvements

--- core/src/main/java/io/javaoperatorsdk/admissioncontroller/mutation/Mutator.java
+++ core/src/main/java/io/javaoperatorsdk/admissioncontroller/mutation/Mutator.java
@@ -1,9 +1,10 @@
 package io.javaoperatorsdk.admissioncontroller.mutation;
 
+import io.fabric8.kubernetes.api.model.KubernetesResource;
 import io.javaoperatorsdk.admissioncontroller.NotAllowedException;
 import io.javaoperatorsdk.admissioncontroller.Operation;
 
-public interface Mutator<T> {
+public interface Mutator<T extends KubernetesResource> {
 
   T mutate(T resource, Operation operation) throws NotAllowedException;
 
--- core/src/main/java/io/javaoperatorsdk/admissioncontroller/validation/Validator.java
+++ core/src/main/java/io/javaoperatorsdk/admissioncontroller/validation/Validator.java
@@ -1,9 +1,10 @@
 package io.javaoperatorsdk.admissioncontroller.validation;
 
+import io.fabric8.kubernetes.api.model.KubernetesResource;
 import io.javaoperatorsdk.admissioncontroller.NotAllowedException;
 import io.javaoperatorsdk.admissioncontroller.Operation;
 
-public interface Validator<T> {
+public interface Validator<T extends KubernetesResource> {
 
   void validate(T resource, Operation operation) throws NotAllowedException;
 
