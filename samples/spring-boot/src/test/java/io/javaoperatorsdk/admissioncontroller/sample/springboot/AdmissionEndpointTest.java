d195f6c160b47d488dd2ad8994be403e99153652
csviri <csviri@gmail.com>
2022-04-08 14:51:19 +0200

    fix: tests

--- samples/spring-boot/src/test/java/io/javaoperatorsdk/admissioncontroller/sample/springboot/AdmissionEndpointTest.java
+++ samples/spring-boot/src/test/java/io/javaoperatorsdk/admissioncontroller/sample/springboot/AdmissionEndpointTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
-@ContextConfiguration(classes = Config.class)
+@Import(Config.class)
 @WebFluxTest(AdmissionEndpoint.class)
 class AdmissionEndpointTest {
 
@@ -23,20 +23,37 @@ class AdmissionEndpointTest {
   private Resource request;
 
   @Test
-  public void testValidation() throws Exception {
+  void validation() throws Exception {
     webClient.post().uri("/validate").contentType(MediaType.APPLICATION_JSON)
         .body(BodyInserters.fromValue(new String(Files.readAllBytes(request.getFile().toPath()))))
         .exchange()
         .expectStatus().isOk().expectBody().json("{}");
   }
 
-  // @Test
-  // public void testMutation() throws Exception {
-  // mockMvc.perform(post("/mutate").contentType(MediaType.APPLICATION_JSON)
-  // .content(new String(Files.readAllBytes(request.getFile().toPath()))))
-  // .andExpectAll(content().contentType(MediaType.APPLICATION_JSON),
-  // content().json(
-  // "{\"apiVersion\":\"admission.k8s.io/v1\",\"kind\":\"AdmissionReview\",\"response\":{\"allowed\":true,\"patch\":\"W3sib3AiOiJhZGQiLCJwYXRoIjoiL21ldGFkYXRhL2xhYmVscy9hcHAua3ViZXJuZXRlcy5pb34xbmFtZSIsInZhbHVlIjoibXV0YXRpb24tdGVzdCJ9XQ==\",\"patchType\":\"JSONPatch\",\"uid\":\"0df28fbd-5f5f-11e8-bc74-36e6bb280816\"}}"));
-  // }
+  @Test
+  void mutation() throws Exception {
+    webClient.post().uri("/mutate").contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new String(Files.readAllBytes(request.getFile().toPath()))))
+        .exchange()
+        .expectStatus().isOk().expectBody().json(
+            "{\"apiVersion\":\"admission.k8s.io/v1\",\"kind\":\"AdmissionReview\",\"response\":{\"allowed\":true,\"patch\":\"W3sib3AiOiJhZGQiLCJwYXRoIjoiL21ldGFkYXRhL2xhYmVscy9hcHAua3ViZXJuZXRlcy5pb34xbmFtZSIsInZhbHVlIjoibXV0YXRpb24tdGVzdCJ9XQ==\",\"patchType\":\"JSONPatch\",\"uid\":\"0df28fbd-5f5f-11e8-bc74-36e6bb280816\"}}");
+  }
+
+  @Test
+  void asyncValidation() throws Exception {
+    webClient.post().uri("/async-validate").contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new String(Files.readAllBytes(request.getFile().toPath()))))
+        .exchange()
+        .expectStatus().isOk().expectBody().json("{}");
+  }
+
+  @Test
+  void asyncMutation() throws Exception {
+    webClient.post().uri("/async-mutate").contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(new String(Files.readAllBytes(request.getFile().toPath()))))
+        .exchange()
+        .expectStatus().isOk().expectBody().json(
+            "{\"apiVersion\":\"admission.k8s.io/v1\",\"kind\":\"AdmissionReview\",\"response\":{\"allowed\":true,\"patch\":\"W3sib3AiOiJhZGQiLCJwYXRoIjoiL21ldGFkYXRhL2xhYmVscy9hcHAua3ViZXJuZXRlcy5pb34xbmFtZSIsInZhbHVlIjoibXV0YXRpb24tdGVzdCJ9XQ==\",\"patchType\":\"JSONPatch\",\"uid\":\"0df28fbd-5f5f-11e8-bc74-36e6bb280816\"}}");
+  }
 
 }
