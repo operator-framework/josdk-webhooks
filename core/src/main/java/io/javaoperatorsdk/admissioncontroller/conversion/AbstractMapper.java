6c81ab33d3411c73449cb510c02a029042331de4
csviri <csviri@gmail.com>
2022-07-01 11:10:01 +0200

    naming

--- core/src/main/java/io/javaoperatorsdk/admissioncontroller/conversion/AbstractMapper.java
+++ core/src/main/java/io/javaoperatorsdk/admissioncontroller/conversion/AbstractMapper.java
@@ -2,12 +2,12 @@ package io.javaoperatorsdk.admissioncontroller.conversion;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 
-public abstract class DefaultMapper<S extends HasMetadata, T extends HasMetadata>
+public abstract class AbstractMapper<S extends HasMetadata, T extends HasMetadata>
     implements Mapper<S, T> {
 
   private final String version;
 
-  protected DefaultMapper(String version) {
+  protected AbstractMapper(String version) {
     this.version = version;
   }
 
