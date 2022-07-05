f765a28da15bfd1e9204ef2e1c4a6174d49217ee
csviri <csviri@gmail.com>
2022-07-05 12:09:05 +0200

    fix: snapshot release issue

--- core/src/main/java/io/javaoperatorsdk/webhook/conversion/TargetVersion.java
+++ core/src/main/java/io/javaoperatorsdk/webhook/conversion/TargetVersion.java
@@ -13,6 +13,8 @@ public @interface TargetVersion {
    * The target version of the resource this mapper supports. Example values: "v1","v1beta1". This
    * is not the full API Version just the version suffix, for example only the "v1" of api version:
    * "apiextensions.k8s.io/v1"
+   * 
+   * @return version
    **/
   String value();
 
