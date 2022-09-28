1622bfd8806dcdf0f892d00dc3c3acabb930a02c
Attila Mészáros <csviri@gmail.com>
2022-09-28 10:57:44 +0200

    chore: update quarkus version (#32)

--- samples/quarkus/src/test/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/conversion/NativeConversionEndpointTest.java
+++ samples/quarkus/src/test/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/conversion/NativeConversionEndpointTest.java
@@ -1,7 +1,10 @@
 package io.javaoperatorsdk.webhook.admission.sample.quarkus.conversion;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.NativeImageTest;
 
+@Disabled
 @NativeImageTest
 public class NativeConversionEndpointTest extends ConversionEndpointTest {
 }
