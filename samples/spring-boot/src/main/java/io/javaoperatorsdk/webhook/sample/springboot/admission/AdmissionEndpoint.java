237d19113687f99e2ca4b3323cc16367508bb5e3
Attila Mészáros <csviri@gmail.com>
2022-11-21 11:25:03 +0100

    refactor: separate error case tests from sample (#40)

--- samples/spring-boot/src/main/java/io/javaoperatorsdk/webhook/sample/springboot/admission/AdmissionEndpoint.java
+++ samples/spring-boot/src/main/java/io/javaoperatorsdk/webhook/sample/springboot/admission/AdmissionEndpoint.java
@@ -19,40 +19,24 @@ public class AdmissionEndpoint {
 
   public static final String MUTATE_PATH = "mutate";
   public static final String VALIDATE_PATH = "validate";
-  public static final String ERROR_MUTATE_PATH = "error-mutate";
-  public static final String ERROR_VALIDATE_PATH = "error-validate";
   public static final String ASYNC_MUTATE_PATH = "async-mutate";
   public static final String ASYNC_VALIDATE_PATH = "async-validate";
-  public static final String ERROR_ASYNC_MUTATE_PATH = "error-async-mutate";
-  public static final String ERROR_ASYNC_VALIDATE_PATH = "error-async-validate";
 
   private final AdmissionController<Pod> mutatingController;
   private final AdmissionController<Pod> validatingController;
-  private final AdmissionController<Pod> errorMutatingController;
-  private final AdmissionController<Pod> errorValidatingController;
   private final AsyncAdmissionController<Pod> asyncMutatingController;
   private final AsyncAdmissionController<Pod> asyncValidatingController;
-  private final AsyncAdmissionController<Pod> errorAsyncMutatingController;
-  private final AsyncAdmissionController<Pod> errorAsyncValidatingController;
 
   @Autowired
   public AdmissionEndpoint(
       @Qualifier("mutatingController") AdmissionController<Pod> mutationController,
       @Qualifier("validatingController") AdmissionController<Pod> validatingController,
-      @Qualifier("errorMutatingController") AdmissionController<Pod> errorMutatingController,
-      @Qualifier("errorValidatingController") AdmissionController<Pod> errorValidatingController,
       @Qualifier("asyncMutatingController") AsyncAdmissionController<Pod> asyncMutatingController,
-      @Qualifier("asyncValidatingController") AsyncAdmissionController<Pod> asyncValidatingController,
-      @Qualifier("errorAsyncMutatingController") AsyncAdmissionController<Pod> errorAsyncMutatingController,
-      @Qualifier("errorAsyncValidatingController") AsyncAdmissionController<Pod> errorAsyncValidatingController) {
+      @Qualifier("asyncValidatingController") AsyncAdmissionController<Pod> asyncValidatingController) {
     this.mutatingController = mutationController;
     this.validatingController = validatingController;
-    this.errorMutatingController = errorMutatingController;
-    this.errorValidatingController = errorValidatingController;
     this.asyncMutatingController = asyncMutatingController;
     this.asyncValidatingController = asyncValidatingController;
-    this.errorAsyncMutatingController = errorAsyncMutatingController;
-    this.errorAsyncValidatingController = errorAsyncValidatingController;
   }
 
   @PostMapping(MUTATE_PATH)
@@ -67,18 +51,6 @@ public class AdmissionEndpoint {
     return validatingController.handle(admissionReview);
   }
 
-  @PostMapping(ERROR_MUTATE_PATH)
-  @ResponseBody
-  public AdmissionReview errorMutate(@RequestBody AdmissionReview admissionReview) {
-    return errorMutatingController.handle(admissionReview);
-  }
-
-  @PostMapping(value = ERROR_VALIDATE_PATH)
-  @ResponseBody
-  public AdmissionReview errorValidate(@RequestBody AdmissionReview admissionReview) {
-    return errorValidatingController.handle(admissionReview);
-  }
-
   @PostMapping(ASYNC_MUTATE_PATH)
   @ResponseBody
   public Mono<AdmissionReview> asyncMutate(@RequestBody AdmissionReview admissionReview) {
@@ -89,18 +61,6 @@ public class AdmissionEndpoint {
   @ResponseBody
   public Mono<AdmissionReview> asyncValidate(@RequestBody AdmissionReview admissionReview) {
     return Mono.fromCompletionStage(asyncValidatingController.handle(admissionReview));
-  }
-
-  @PostMapping(ERROR_ASYNC_MUTATE_PATH)
-  @ResponseBody
-  public Mono<AdmissionReview> errorAsyncMutate(@RequestBody AdmissionReview admissionReview) {
-    return Mono.fromCompletionStage(errorAsyncMutatingController.handle(admissionReview));
-  }
-
-  @PostMapping(ERROR_ASYNC_VALIDATE_PATH)
-  @ResponseBody
-  public Mono<AdmissionReview> errorAsyncValidate(@RequestBody AdmissionReview admissionReview) {
-    return Mono.fromCompletionStage(errorAsyncValidatingController.handle(admissionReview));
   }
 
 }
