fa7a720e41d2eea1fd95063157579060ffd4e41d
csviri <csviri@gmail.com>
2022-11-22 13:06:11 +0100

    readme

--- samples/quarkus/src/main/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/conversion/ConversionControllerConfig.java
+++ samples/quarkus/src/main/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/conversion/ConversionControllerConfig.java
@@ -5,12 +5,17 @@ import javax.inject.Singleton;
 import io.javaoperatorsdk.webhook.conversion.AsyncConversionController;
 import io.javaoperatorsdk.webhook.conversion.ConversionController;
 import io.javaoperatorsdk.webhook.sample.commons.ConversionControllers;
+import io.javaoperatorsdk.webhook.sample.commons.mapper.V1Mapper;
+import io.javaoperatorsdk.webhook.sample.commons.mapper.V2Mapper;
 
 public class ConversionControllerConfig {
 
   @Singleton
   public ConversionController conversionController() {
-    return ConversionControllers.conversionController();
+    var controller = new ConversionController();
+    controller.registerMapper(new V1Mapper());
+    controller.registerMapper(new V2Mapper());
+    return controller;
   }
 
   @Singleton
