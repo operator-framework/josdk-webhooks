b64d14eda02c331e6fbfb9ff9dfff88ec3e4b709
csviri <csviri@gmail.com>
2022-12-15 15:02:05 +0100

    wip

--- samples/quarkus/src/test/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/admission/AdmissionControllerE2E.java
+++ samples/quarkus/src/test/java/io/javaoperatorsdk/webhook/admission/sample/quarkus/admission/AdmissionControllerE2E.java
@@ -31,7 +31,7 @@ class AdmissionControllerE2E {
                 "https://github.com/cert-manager/cert-manager/releases/download/v1.10.1/cert-manager.yaml")
                     .openStream()) {
       applyAndWait(client, certManager);
-      // applyAndWait(client, "target/kubernetes/minikube.yml");
+      applyAndWait(client, "target/kubernetes/minikube.yml");
       applyAndWait(client, "k8s/validating-webhook-configuration.yml");
       applyAndWait(client, "k8s/mutating-webhook-configuration.yml");
     }
