7a455b52efcd81d4c4dd365ec2e6d89cb9fbeacf
csviri <csviri@gmail.com>
2022-12-18 21:47:55 +0100

    sample refactor

--- samples/commons/src/main/java/io/javaoperatorsdk/webhook/sample/commons/customresource/MultiVersionCustomResourceSpecV2.java
+++ samples/commons/src/main/java/io/javaoperatorsdk/webhook/sample/commons/customresource/MultiVersionCustomResourceSpecV2.java
@@ -2,15 +2,14 @@ package io.javaoperatorsdk.webhook.sample.commons.customresource;
 
 public class MultiVersionCustomResourceSpecV2 {
 
-  private String value;
+  private String changedValueName;
 
-
-  public String getValue() {
-    return value;
+  public String getChangedValueName() {
+    return changedValueName;
   }
 
-  public MultiVersionCustomResourceSpecV2 setValue(String value) {
-    this.value = value;
+  public MultiVersionCustomResourceSpecV2 setChangedValueName(String changedValueName) {
+    this.changedValueName = changedValueName;
     return this;
   }
 
