ecb13dde82db6e032d53dfa55be46198c6fbb229
Attila Mészáros <csviri@gmail.com>
2025-05-28 13:07:22 +0200

    fix: e2e tests (#331)
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- samples/commons/src/main/java/io/javaoperatorsdk/webhook/sample/commons/Utils.java
+++ samples/commons/src/main/java/io/javaoperatorsdk/webhook/sample/commons/Utils.java
@@ -9,18 +9,25 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.Event;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.apiextensions.v1.*;
 import io.fabric8.kubernetes.api.model.networking.v1.*;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientTimeoutException;
+import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
 
 import static io.javaoperatorsdk.webhook.sample.commons.AdmissionControllers.VALIDATION_TARGET_LABEL;
 import static io.javaoperatorsdk.webhook.sample.commons.ConversionControllers.CONVERSION_PATH;
 
 public class Utils {
 
-  public static final int SPIN_UP_GRACE_PERIOD = 120;
+  private static final Logger log = LoggerFactory.getLogger(Utils.class);
 
+  public static final int SPIN_UP_GRACE_PERIOD = 180;
 
   public static void applyAndWait(KubernetesClient client, String path) {
     applyAndWait(client, path, null);
@@ -41,11 +48,25 @@ public class Utils {
 
   public static void applyAndWait(KubernetesClient client, List<HasMetadata> resources,
       UnaryOperator<HasMetadata> transformer) {
-    if (transformer != null) {
-      resources = resources.stream().map(transformer).collect(Collectors.toList());
+    try {
+      if (transformer != null) {
+        resources = resources.stream().map(transformer).collect(Collectors.toList());
+      }
+      client.resourceList(resources).createOrReplace();
+      client.resourceList(resources).waitUntilReady(5, TimeUnit.MINUTES);
+    } catch (KubernetesClientTimeoutException e) {
+      log.info("Timed out resource list: {}", client.resourceList(resources).get());
+      var deployment = client.resourceList(resources).get().stream()
+          .filter(r -> r.getKind().equals("Deployment")).findFirst().orElseThrow();
+      log.info("Deployment:\n {} \n",
+          new KubernetesSerialization().asYaml(deployment));
+
+      client.v1().events().inNamespace(deployment.getMetadata().getNamespace()).list().getItems()
+          .stream()
+          .map(Event::getMessage)
+          .forEach(ev -> log.info("Event: {}", new KubernetesSerialization().asYaml(ev)));
+      throw e;
     }
-    client.resourceList(resources).createOrReplace();
-    client.resourceList(resources).waitUntilReady(3, TimeUnit.MINUTES);
   }
 
   public static void applyAndWait(KubernetesClient client, InputStream is,
